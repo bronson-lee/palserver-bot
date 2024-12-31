3aeff8dd24dd36f04a9ce62a0fd31aceb00aa189
Bronson Lee <bkl263@nyu.edu>
2024-12-31 01:50:20 -0800

    Prevent stop when players online

--- commands/stop.ts
+++ commands/stop.ts
@@ -1,20 +1,28 @@
-import { ActivityType, type CacheType, type ChatInputCommandInteraction, type Client } from "discord.js"
+import { ActivityType, Message } from "discord.js"
+import type { CacheType, ChatInputCommandInteraction, Client } from 'discord.js'
 import { isServerOnline, stopServer } from "../service/apiService"
 import { ServerStatus } from "../enums"
+import { getPlayers } from "../connectors/palworldConnector"
 export default <Command>{
     name: 'stop',
     description: 'Stop the Palworld server.',
     requiresLock: true,
-    action: async (interaction : ChatInputCommandInteraction<CacheType>, client : Client<boolean> ) => {
+    action: async (interaction : ChatInputCommandInteraction<CacheType>, client : Client<boolean> ) : Promise<Message> => {
         return isServerOnline()
         .then(async (isOnline) => {
             if(isOnline) {
-                await stopServer()
-                client.user?.setActivity({
-                    name: ServerStatus.OFFLINE,
-                    type: ActivityType.Custom
-                })
-                await interaction.editReply("Server is now offline.")
+                const playersOnline = await getPlayers()
+                const playersOnlineCount = playersOnline.players.length
+                if(playersOnlineCount) {
+                    return interaction.editReply(`Could not stop server. There ${playersOnlineCount == 1 ? 'is' : 'are'} still ${playersOnlineCount} player${playersOnlineCount > 1 ? 's' : ''} online`)
+                } else {
+                    await stopServer()
+                    client.user?.setActivity({
+                        name: ServerStatus.OFFLINE,
+                        type: ActivityType.Custom
+                    })
+                    return interaction.editReply("Server is now offline.")
+                }
             } else {
                 return interaction.editReply("Server is already offline.")
             }
